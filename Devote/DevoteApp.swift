870a4127e3e35fe0e95c224450b89bf752552337
gizemdemirtaass <116717342+gizemdemirtas@users.noreply.github.com>
2024-09-24 13:08:25 +0300

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by gizem demirtas on 24.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
