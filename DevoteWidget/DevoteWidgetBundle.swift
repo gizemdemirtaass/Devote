fcd5162cdc7b26a8767bc283d5ad8963c7641aa1
gizemdemirtaass <116717342+gizemdemirtas@users.noreply.github.com>
2024-10-05 00:21:56 +0300

    commit

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by gizem demirtas on 4.10.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
